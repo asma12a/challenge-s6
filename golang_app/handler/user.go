1d1a311dfc4a72ec367f04f714ac4595be03da03
Taslima <taslimaahamedmze@gmail.com>
2024-10-18 18:13:38 +0200

    crud team

--- golang_app/handler/user.go
+++ golang_app/handler/user.go
@@ -171,7 +171,7 @@ func listUsers(ctx context.Context, service service.User) fiber.Handler {
 				ID:    user.ID,
 				Name:  user.Name,
 				Email: user.Email,
-				Roles: user.Role,
+				Roles: user.Roles,
 			}
 		}
 
--- golang_app/server.go
+++ golang_app/server.go
@@ -53,6 +53,7 @@ func main() {
 	handler.SportHandler(api.Group("/sports"), context.Background(), *service.NewSportService(db_client))
 	handler.UserHandler(api.Group("/users"), context.Background(), *service.NewUserService(db_client))
 	handler.EventTeamsHandler(api.Group("/event_teams"), context.Background(), *service.NewEventTeamsService(db_client), *service.NewEventService(db_client), *service.NewTeamService(db_client))
+	handler.TeamHandler(api.Group("/teams"), context.Background(), *service.NewTeamService(db_client))
 
 	// Any other routes: Not Found
 	app.All("*", func(c *fiber.Ctx) error {
