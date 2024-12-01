7a5936f3d581890d741237e809e3e2dce0aea189
Asma MOKEDDES <mokeddes.asma@outlook.fr>
2024-12-01 16:40:39 +0100

    all is ok

--- golang_app/internal/ws/handler.go
+++ golang_app/internal/ws/handler.go
@@ -9,23 +9,58 @@ import (
 // WebSocketHandler handles incoming WebSocket connections
 func WebSocketHandler(hub *Hub) func(*websocket.Conn) {
 	return func(conn *websocket.Conn) {
-		// Register the client in the Hub
-		hub.register <- conn
+		// Authentifier l'utilisateur avant d'autoriser la connexion WebSocket
+		//	req := conn.Locals("request").(*fiber.Request)
+		// token := string(req.Header.Peek("Authorization"))
+		// if token == "" {
+		// 	conn.WriteMessage(websocket.TextMessage, []byte("Erreur: Pas d'authentification"))
+		// 	conn.Close()
+		// 	return
+		// }
 
+		// // Valider le token ici, par exemple avec votre middleware JWT
+		// if !isValidToken(token) {
+		// 	conn.WriteMessage(websocket.TextMessage, []byte("Erreur: Token invalide"))
+		// 	conn.Close()
+		// 	return
+		// }
+
+		// Si l'utilisateur est authentifié, alors procéder
+		hub.register <- conn
 		defer func() {
-			// Unregister the client upon disconnection
 			hub.unregister <- conn
 		}()
 
-		// Listen for messages from the client
 		for {
 			_, message, err := conn.ReadMessage()
 			if err != nil {
 				log.Println("Read error:", err)
 				break
 			}
-			// Broadcast the message to all clients
+
+			// Enregistrez le message dans la base de données
+			// storeMessage(db, message)
+
+			// Diffuser le message à tous les clients
 			hub.Broadcast(message)
 		}
 	}
 }
+
+func isValidToken(token string) bool {
+	// Implémentez ici la logique pour valider le token JWT
+	return true
+}
+
+// func storeMessage(db *gorm.DB, message []byte) {
+// 	// Créez une structure Message avec les informations nécessaires
+// 	msg := models.Message{
+// 		EventID: 1, // L'ID de l'événement ou à récupérer via la connexion
+// 		Content: string(message),
+// 	}
+
+// 	// Insérez le message dans la base de données
+// 	if err := db.Create(&msg).Error; err != nil {
+// 		log.Println("Erreur lors du stockage du message:", err)
+// 	}
+// }
