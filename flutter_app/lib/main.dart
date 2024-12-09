117006ad26b33a5abf24c1acd089c920c935bbb8
dan1M <maneadaniel555@gmail.com>
2024-12-09 14:53:01 +0100

    handle logout, and auth redirection from main if not connected

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:squad_go/screens/sign_in.dart';
 import 'package:squad_go/screens/tabs.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,22 +15,30 @@ final theme = ThemeData(
 );
 
 Future main() async {
+  final storage = const FlutterSecureStorage();
   await dotenv.load(fileName: "assets/../.env");
+  final token = dotenv.env['JWT_STORAGE KEY'] != null
+      ? await storage.read(key: dotenv.env['JWT_STORAGE KEY']!)
+      : null;
+
   runApp(
-    const ProviderScope(
-      child: App(),
+    ProviderScope(
+      child: App(
+        token: token,
+      ),
     ),
   );
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final String? token;
+  const App({super.key, this.token});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const TabsScreen(),
+      home: token != null ? const TabsScreen() : const SignInScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
