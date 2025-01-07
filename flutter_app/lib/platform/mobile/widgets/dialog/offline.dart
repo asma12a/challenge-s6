4b42d04cb98a4e370894ff726d6fd73a06a8aa6b
dan1M <maneadaniel555@gmail.com>
2025-01-07 17:16:43 +0100

    base connectivity handler

---
+++ flutter_app/lib/platform/mobile/widgets/dialog/offline.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OfflineDialog extends StatelessWidget {
+  const OfflineDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
