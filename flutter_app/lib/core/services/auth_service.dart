061eec170bdca9edb1cba966ae1a4524ae76cedc
Asma MOKEDDES <mokeddes.asma@outlook.fr>
2025-01-07 17:32:32 +0100

    wip values

--- flutter_app/lib/core/services/auth_service.dart
+++ flutter_app/lib/core/services/auth_service.dart
@@ -9,7 +9,11 @@ class AuthService {
   final _storage = const FlutterSecureStorage();
 
   Future<Map<String, dynamic>> signIn(body) async {
-      print('signIn  ${dotenv.env['API_BASE_URL']!}');
+    print(
+        'URL  ${String.fromEnvironment('API_BASE_URL', defaultValue: 'marche-pas')}');
+
+    print(
+        'token  ${String.fromEnvironment('JWT_STORAGE_KEY', defaultValue: 'marche-pas')}');
 
     try {
       final uri = Uri.http(dotenv.env['API_BASE_URL']!, 'api/auth/login');
