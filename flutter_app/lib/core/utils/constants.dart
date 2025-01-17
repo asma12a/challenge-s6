782296b9716a03bd9b874059c28f2a3e1b9d87aa
Asma MOKEDDES <mokeddes.asma@outlook.fr>
2025-01-17 01:54:05 +0100

    constants

--- flutter_app/lib/core/utils/constants.dart
+++ flutter_app/lib/core/utils/constants.dart
@@ -3,6 +3,10 @@ abstract class Constants {
     'API_BASE_URL',
     defaultValue: 'https://challenge-s6-1.onrender.com',
   );
+   static const String apiBaseUrlWs = String.fromEnvironment(
+    'API_BASE_URL_WS',
+    defaultValue: 'challenge-s6-1.onrender.com',
+  );
 
   static const String jwtStorageToken = String.fromEnvironment(
     'JWT_STORAGE_KEY',
